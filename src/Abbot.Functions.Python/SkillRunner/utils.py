50fd04a8c84dec5148a454bd78eda6a343c17669
Paul Nakata <paul@pmn.org>
2021-01-29 14:21:57 -0800

    basic wire-up to geocoding api

--- src/Abbot.Functions.Python/SkillRunner/utils.py
+++ src/Abbot.Functions.Python/SkillRunner/utils.py
@@ -5,29 +5,44 @@ import logging
 
 from __app__.SkillRunner.apiclient import ApiClient
 
+class Geocode(object):
+    def __init__(self, coordinate, formatted_address, time_zone_id = None):
+        self.formatted_address = formatted_address
+        self.time_zone_id = time_zone_id
+        self.latitude = coordinate.get("latitude")
+        self.longitude = coordinate.get("longitude")
+    
+    def __repr__(self):
+        return "Geocode: {} ({},{})".format(self.formatted_address, 
+                                            self.latitude, 
+                                            self.longitude)
+
+
 class Utilities(object):
     def __init__(self, skill_id, user_id, api_token, timestamp):
         self.skill_id = skill_id
-        self.request_uri = os.environ.get('SkillApiBaseUriFormatString', 'https://localhost:4979/api/skills/{0}') 
+        endpoint_uri = os.environ.get('SkillApiBaseUriFormatString', 'https://localhost:4979/api/skills/{0}') 
+        self.request_uri = endpoint_uri.format(self.skill_id)
+        
         if self.request_uri.startswith("https://localhost"):
             self.verify_ssl = False
         else:
             self.verify_ssl = True
         self.api_client = ApiClient(self.request_uri, user_id, api_token, timestamp)
 
-
-    def make_uri(self, key):
-        return self.request_uri.format(self.skill_id, key)
-
     
     def geocode(self, address, include_timezone=False):
         clean_address = requests.utils.requote_uri(address)
         uri = self.request_uri + "/geo?address={}&includeTimezone={}".format(clean_address, include_timezone)
         result = self.api_client.get(uri)
-
         # This will return an object that contains a string called Address, and an (int, int) tuple called Geocode 
         # representing the lat/lng of the point.
-        return result
+        # example:
+        # {'timeZoneId': 'America/Los_Angeles', 
+        #  'coordinate': {'latitude': 34.0692042, 'longitude': -118.4066574}, 
+        #  'formattedAddress': '9663 S Santa Monica Blvd, Beverly Hills, CA 90210, USA'}
+        g = Geocode(result.get("coordinate"), result.get("formattedAddress"), result.get("timeZoneId"))
+        return g
 
 
     def __str__(self):
