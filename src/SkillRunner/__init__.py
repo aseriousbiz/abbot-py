8839fda17510c99c10ac70d15a784c6f554dbc47
Paul Nakata <paul@pmn.org>
2021-03-03 14:56:39 -0800

    bump version for testing purposes

--- src/SkillRunner/__init__.py
+++ src/SkillRunner/__init__.py
@@ -22,7 +22,7 @@ def run_code(req, api_token):
 def main(req: func.HttpRequest) -> func.HttpResponse:
     if req.method == "GET":
         return func.HttpResponse(
-            body=json.dumps(["Ok! Running Abbot Python Runner v0.2.0."]),
+            body=json.dumps(["Ok! Running Abbot Python Runner v0.2.1."]),
             mimetype="text/json",
             status_code=200
         )
