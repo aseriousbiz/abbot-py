c58959c7bc7fbdbd810a0c1734c8d94c8badb3c0
Phil Haack <haacked@gmail.com>
2021-07-22 16:43:03 -0700

    Add a TriggerResponse property to bot

--- src/SkillRunner/bot/bot.py
+++ src/SkillRunner/bot/bot.py
@@ -21,7 +21,6 @@ from .utils import obj
 from . import exceptions
 from . import apiclient
 
-
 class TriggerEvent(object):
     """
     A request triggered by an external event.
@@ -49,6 +48,56 @@ class TriggerEvent(object):
 
     def toJSON(self):
         return jsonpickle.encode(self)
+
+class TriggerResponse(object):
+    """
+    A response to an HTTP request triggered by an external event.
+
+    :var raw_content: The raw content to return as the body of the response. Cannot be set if content is set.
+    :var content: The content to return as the body of the response. This will be serialized as JSON. Cannot be set if raw_content is set.
+    :var content_type: The Content Type of the response. If null, Abbot will choose the best content type using content negotiation.
+    :var headers: The request Headers as defined in RFC 2616 that should be sent in the response.
+    """
+    def __init__(self):
+        self.headers = {}
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, value):
+        self._content = value
+        self._raw_content = jsonpickle.encode(value)
+
+    @content.deleter
+    def content(self):
+        del self._content
+
+    @property
+    def raw_content(self):
+        return self._raw_content
+
+    @raw_content.setter
+    def raw_content(self, value):
+        self._content = None
+        self._raw_content = value
+
+    @content.deleter
+    def raw_content(self):
+        del self._raw_content
+
+    @property
+    def content_type(self):
+        return self._content_type
+
+    @content_type.setter
+    def content_type(self, value):
+        self._content_type = value
+
+    @content_type.deleter
+    def content_type(self):
+        del self._content_type
 
 
 class Mention(object):
@@ -239,6 +288,7 @@ class Bot(object):
         
         if self.is_request:
             self.request = TriggerEvent(skillInfo.get('Request'))
+            self.response = TriggerResponse()
         else:
             self.request = None
 
