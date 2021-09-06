62857ee9de250ff907817d5975a4a2f68a99dc6e
Andreia Gaita <shana@spoiledcat.net>
2021-09-06 16:00:11 +0100

    Reenable everything, let's see how this works

--- .github/deploy_fly.sh
+++ .github/deploy_fly.sh
@@ -8,11 +8,16 @@ if [ -z $1 ]; then
 fi
 
 if [ -z $2 ]; then
-  echo "Tag cannot be empty!"
+  echo "Source tag cannot be empty!"
   exit 1
 fi
 
-curl -X POST https://api.github.com/repos/aseriousbiz/docker/actions/workflows/12816714/dispatches \
+if [ -z $3 ]; then
+  echo "Target tag cannot be empty!"
+  exit 1
+fi
+
+curl -X POST https://api.github.com/repos/aseriousbiz/docker/actions/workflows/12887088/dispatches \
 -H "Content-Type: application/json" -H 'Accept: application/vnd.github.everest-preview+json' \
 -H "Authorization: token ${GH_BOT_TOKEN}" \
---data "{ \"ref\": \"main\", \"inputs\": {\"runner\": \"$1\", \"tag\": \"$2\"} }"
+--data "{ \"ref\": \"main\", \"inputs\": {\"runner\": \"$1\", \"source_tag\": \"$2\", \"target_tag\": \"$3\" } }"
