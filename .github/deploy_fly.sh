e5ca0e30ab117296164cd5a1bbc8bb60be2ed1e0
Andreia Gaita <shana@spoiledcat.net>
2021-09-22 17:43:33 +0100

    Streamline container deployment pipeline

--- .github/deploy_fly.sh
+++ .github/deploy_fly.sh
@@ -8,16 +8,11 @@ if [ -z $1 ]; then
 fi
 
 if [ -z $2 ]; then
-  echo "Source tag cannot be empty!"
+  echo "Tag cannot be empty!"
   exit 1
 fi
 
-if [ -z $3 ]; then
-  echo "Target tag cannot be empty!"
-  exit 1
-fi
-
-curl -X POST https://api.github.com/repos/aseriousbiz/docker/actions/workflows/12887088/dispatches \
+curl -X POST https://api.github.com/repos/aseriousbiz/docker/actions/workflows/12816714/dispatches \
 -H "Content-Type: application/json" -H 'Accept: application/vnd.github.everest-preview+json' \
 -H "Authorization: token ${GH_BOT_TOKEN}" \
---data "{ \"ref\": \"main\", \"inputs\": {\"runner\": \"$1\", \"source_tag\": \"$2\", \"target_tag\": \"$3\" } }"
+--data "{ \"ref\": \"main\", \"inputs\": {\"runner\": \"$1\", \"tag\": \"$2\" } }"
