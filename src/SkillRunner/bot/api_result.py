9ca7eb14a3a866e3dc9391270292d093a2e39a1e
Phil Haack <haacked@gmail.com>
2021-11-01 11:49:21 -0700

    Implement a simple `ApiResult` class

---
+++ src/SkillRunner/bot/api_result.py
@@ -0,0 +1,12 @@
+import logging
+
+class ApiResult(object):
+    """
+    A class to represent the result of an API call.
+    """
+    def __init__(self, response):
+        logging.info(f"ApiResult: {response}")
+        self.ok = response.get('ok', False)
+        """ Whether the operation succeeded. """
+        self.error = response.get('error', None)
+        """ The error message if the operation failed. """
