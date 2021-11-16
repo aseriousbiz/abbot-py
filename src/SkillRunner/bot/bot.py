6f06bcd4ae67aecfc3f2a22ae28eff30057a1e17
Phil Haack <haacked@gmail.com>
2021-11-15 18:05:01 -0800

    Move button to its own file

--- src/SkillRunner/bot/bot.py
+++ src/SkillRunner/bot/bot.py
@@ -31,6 +31,7 @@ from .reply_client import ReplyClient
 from . import exceptions
 from .apiclient import ApiClient
 from .trigger_response import TriggerResponse
+from .button import Button
 from types import SimpleNamespace
 
 
@@ -63,27 +64,6 @@ class MentionArgument(Argument):
 
     def __str__(self):
         return "mentioned: {}".format(self.mentioned)
-
-
-class Button(object):
-    """
-    A button presented to the user.
-
-    :var title: The title displayed on the button.
-    :var args: The arguments to pass back to this skill when the button is clicked.
-    :var style: (optional) The style to apply to the button. Allowed values are 'default', 'primary', and 'danger'. Use 'primary' and 'danger' sparingly.
-    """
-    def __init__(self, title, args=None, style="default"):
-        self.title = title
-        self.arguments = args if args is not None else title
-        self.style = style
-
-    def toJSON(self):
-        return {
-            "Title": self.title,
-            "Arguments": self.arguments,
-            "Style": self.style
-        }
 
 
 class Bot(object):
